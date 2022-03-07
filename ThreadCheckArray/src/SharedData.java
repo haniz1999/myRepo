fa150c00b4bc05d70e79fa5ff1b9ab75ddecb132
haniz1999 <hanizahran3@gmail.com>
2022-03-07 23:09:27 +0200

    test

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class SharedData 
 {
-	private ArrayList<Integer> list;//zeb
+	private ArrayList<Integer> list;//ze
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
